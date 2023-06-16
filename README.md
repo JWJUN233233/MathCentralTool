ed55407a600f473d9c57a93e8bcff8f0b383946d
FTS427 <FTS427@outlook.com>
2023-06-16 21:28:41 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# MathCentralTool
-一个开源的，基于终端的，无GUI的数学小工具
+<div align="center">
+    <img width="175" src="/other/icon/MCT.png"></img>
+</div>
+
+<h1 align="center">MathCentralTools</h1>
+
+<div align="center">
+
+![下载](https://img.shields.io/github/downloads/FTS427/MathCentralTools/total?logo=github&label=下载&style=for-the-badge&color=44cc11)
+![星标](https://img.shields.io/github/stars/FTS427/MathCentralTools?logo=github&label=星标&style=for-the-badge)
+![协议](https://img.shields.io/github/license/FTS427/MathCentralTools?logo=github&label=协议&style=for-the-badge&color=ff7a35)
+![提议](https://img.shields.io/github/issues/FTS427/MathCentralTools?logo=github&lable=提议&style=for-the-badge "提议")
+![版本](https://img.shields.io/github/v/tag/FTS427/MathCentralTools?label=版本&style=for-the-badge "版本")
+
+![Archlinux](https://img.shields.io/badge/archlinux-1E90FF?style=for-the-badge&logo=archlinux&logoColor=white)
+![Windows](https://img.shields.io/badge/windows-4682B4?style=for-the-badge&logo=windows&logoColor=white)
+
+![C++](https://img.shields.io/badge/c++-4B0082?style=for-the-badge&logoColor=white)
+
+![邮箱](https://img.shields.io/badge/邮箱-fts427%40outlook.com-4169E1?style=for-the-badge&logoColor=white)
+![QQ](https://img.shields.io/badge/qq-2783629533-B0C4DE?style=for-the-badge&logoColor=white)
+![Bilibili](https://img.shields.io/badge/Bilibili-探索者FTS-FFC0CB?style=for-the-badge&logo=bilibili&logoColor=white)
+![信息](https://repobeats.axiom.co/api/embed/cc935f853d439a32eda4eca113063598a914889a.svg "仓库情况图")
+
+</div>
+
+## 📃介绍
+
+一个似乎没有什么用的数学工具，一个基于终端，没有GUI的小工具。
+
+本程序是免费开源的，此源代码可以随意使用。允许任何人使用源代码进行扩充，编译等操作。出现任何问题，本人不承担任何责任！
+
+⚠注意：本程序处于开发阶段，不可用于科学的精密计算！如果出现任何问题，后果自负！
+
+## ❔如何使用
+
+自行拉取编译，Linux和Windows的源代码不一样，要注意！
+
+## 🧑‍💻编写人员
+
+[FTS427](https://github.com/FTS427)
+
+注：本人初二学生，更新较慢，程序功能较少，请见谅。您如果愿意加入我或者有问题，可电邮：<FTS427@outlook.com>
+
+## 👀其他
+
+可以关注我的B站账号：[探索者FTS](https://space.bilibili.com/1978537245?spm_id_from=333.1007.0.0)
+
+我的QQ：2783629533
---
+++ src/Stable/MCT-Windows.cpp
@@ -0,0 +1,310 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+char q;
+
+void clear()
+{
+    system("CLS");
+}
+
+void print(const char a[],int i)
+{
+    if(i == 0)
+        cout << a ;
+    else if(i == 1)
+        cout << a << endl;
+    else
+        cout << "Code includes BUG!" << endl;
+}
+
+void ERROR()
+{
+	clear();
+    print("\033[37;43;1mERROR!\033[0m",1);
+}
+
+void h1()
+{
+    print("--------------------",1);
+}
+
+void h2()
+{
+    print("*==================*",1);
+}
+
+void s(const char a[])
+{
+    cout << " - " << a << endl;
+}
+
+void h(const char a[])
+{
+    cout << "欢迎使用" << a << "功能！" << endl;
+}
+
+//calc函数,计算器
+void calc()
+{
+    clear();
+    char o;
+    long int num1, num2;
+    long double num3;
+    h("计算器");
+    print("\033[1;37;43m注意\033[0m：现仅只支持两个数之间的运算！",1);
+	while (true)
+    {
+        h1();
+        print("请输入计算式(+ - * / ^),输入'0c0'退出",1);
+        print("如: 1+1",1);
+        cin >> num1 >> o >> num2;
+        //加法
+        if (o == '+')
+            cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+        //减法
+        else if (o == '-')
+            cout << num1 << "-" << num2 << "=" << num1 - num2 << endl;
+        //乘法
+        else if (o == '*')
+            cout << num1 << "*" << num2 << "=" << num1 * num2 << endl;
+        //除法
+        else if (o == '/')
+        {
+            if (num2 == 0)
+                ERROR();
+            else
+                cout << num1 << "/" << num2 << "=" << num1/num2 << endl;
+        }
+        //乘方
+        else if (o == '^') 
+        {
+            long int n=num2;
+			if (num1 == 0 && num2 == 0) 
+                ERROR(); 
+            else 
+            { 
+                num3 = 1; 
+                while (num2 > 0) 
+                { 
+                    num3 *= num1; 
+                    num2--; 
+                } 
+            } 
+            cout << num1 << "^" << n << "=" << num3 << endl; 
+        }
+        //退出
+        else if(o == 'c' && num1 == 0 && num2 ==0)
+        {
+            clear();
+            break;
+        }
+        //报错
+        else
+			ERROR();
+	}
+}
+
+//B函数,计算最大公约数
+void B()
+{
+    clear();
+    long double num3, num4;
+    h("最大公约数");
+    print("\033[1;43;37m注意\033[0m：本功能只支持两个数之间的运算！",1);
+    while(true)
+    {
+        h1();
+        print("输入任意值继续，输入“\033[33;1mc\033[0m”退出...",1);
+        cin >> q;
+        if(q == 'c' || q == 'C')
+        {
+            clear();
+            break;
+        }
+        else
+        {
+            print("请输入第一个数:",0);
+            cin >> num3;
+            print("请输入第二个数:",0);
+            cin >> num4;
+            num3 = abs(num3);
+            num4 = abs(num4);
+            while (num3 != num4)
+            {
+                if (num3 > num4)
+                    num3 -= num4;
+                else
+                    num4 -= num3;
+            }
+            cout << "这两个数的的最大公约数是: " << num3 + num4 << endl;
+        }
+    }
+}
+
+//C函数,计算最小公倍数
+void C()
+{
+    clear();
+    h("最小公倍数");
+    print("\033[1;43;37m注意\033[0m：本功能只支持两个数之间的运算!",1);
+    while(true)
+    {
+        h1();
+        print("输入任意值继续，输入“\033[33;1mc\033[0m”退出...",1);
+        cin >> q;
+        if(q == 'c' || q == 'C')
+        {
+            clear();
+            break;
+        }
+        else
+        {
+            long int n3;
+            long double num5, num6;
+            while (true)
+            {
+                print("请输入第一个数:",0);
+                cin >> num5;
+                print("请输入第二个数:",0);
+                cin >> num6;
+                if (num5 != int(num5) || num6 != int(num6))
+                    ERROR();
+                else
+                    break;
+            }
+            n3 = (num5 > num6) ? num5 : num6;
+            while (true)
+            {
+                if (n3 % int(num5) == 0 && n3 % int(num6) == 0)
+                {
+                    cout << "这两个数的的最小公倍数是：" << n3 << endl;
+                    break;
+                }
+                else
+		            ++n3;
+            }
+        }
+    }
+}
+
+
+//D函数,数值分析器
+void D()
+{
+    clear();
+    long double x, a;
+    h("数值分析器");
+    while(true)
+    {
+        h1();
+        print("输入任意键继续，输入“\033[1;33mc\033[0m”退出...",1);
+        cin >> q;
+        if (q == 'c' || q == 'C')
+        {
+            clear();
+            break;
+        }
+        else
+        {
+            print("请输入一个数:",0);
+            cin >> x;
+            if (x == 0)
+            {
+                cout << x << "是整数" << endl;
+                cout << x << "是正数" << endl;
+                cout << x << "是不分质数合数" << endl;
+                cout << x << "是偶数" << endl;
+            }
+            else
+            {
+                if (modf(x, &a) == 0.0)
+                    cout << x << "是整数" << endl;
+                else
+                    cout << x << "是分数" << endl;
+                if (x > 0)
+                    cout << x << "是正数" << endl;
+                else
+                    cout << x << "是负数" << endl;
+                if (x == 1)
+                    cout << x << "是不分质数合数" << endl;
+                else if (int(x) - x != 0) 
+                    cout << x << "是不分质数合数" << endl;
+                else
+                {
+                    int n=0, i;
+                    for(i=2;i<x;i=i+1)
+                    if(int(x)%1 == 0)
+                        n=n+1;
+                    if (n >0)
+                        cout << x << "是合数" << endl;
+                    else
+                        cout << x << "是质数" << endl;
+                }
+                if (int(x) == x)
+                {
+                    if (int(x) % 2 == 0)
+                        cout << x << "是偶数" << endl;
+                    else
+                        cout << x << "是奇数" << endl;
+                }
+                else
+                {
+                    if (x/2 - int(x)/2 == 0)
+                        cout << x << "是偶数" << endl;
+                    else
+                        cout << x << "是奇数" << endl;
+                }
+            }
+        }
+    }
+}
+
+//M函数,简化主函数
+void M()
+{
+    h("集成数学工具");
+    char u;
+    while(true)
+    {
+        h1();
+        s("\033[1;34m(1)\033[34m计算器\033[0m");
+        s("\033[35;1m(2)\033[35m最大公约数\033[0m");
+        s("\033[1;36m(3)\033[36m最小公倍数\033[0m");
+        s("\033[1;32m(4)\033[32m数值分析器\033[0m");
+        s("\033[0m(a)关于\033[0m");
+        s("\033[1;33m(e)退出\033[0m");
+        print("请键入所需功能：",0);
+        cin >> u;
+        if (u == '1')
+            calc();
+        else if (u == '2')
+            B();
+        else if (u == '3')
+            C();
+        else if (u == '4')
+            D();
+		else if (u == 'E' || u == 'e')
+		    break;
+        else if (u == 'A'  || u == 'a' )
+        {
+            clear();
+            h2();
+            s("MCT Stable v0.0.1.1");
+            s("Made by FTS427");
+            s("Github : https://github.com/FTS427/MathCentTools");
+            h2();
+        }
+        else
+            ERROR();
+    }
+}
+
+//主函数部分
+int main()
+{
+    clear();
+	M();
+	return 0;
+}
